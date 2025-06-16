b020c0b74b887c4028c6a5d50e1c1cc8db7fc6ed
LumAI <zurichixbt@gmail.com>
2025-06-16 23:07:29 +0200

    Add lib/utils.ts

---
+++ lib/utils.ts
@@ -0,0 +1,23 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatPrice(price: number): string {
+  return new Intl.NumberFormat('en-US', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 4,
+  }).format(price)
+}
+
+export function formatVolume(volume: number): string {
+  if (volume >= 1000000) {
+    return `${(volume / 1000000).toFixed(1)}M`
+  }
+  if (volume >= 1000) {
+    return `${(volume / 1000).toFixed(1)}K`
+  }
+  return volume.toString()
+}
