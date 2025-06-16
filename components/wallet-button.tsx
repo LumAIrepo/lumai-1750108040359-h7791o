65bd9533171be860a2757ad51bdfb92a7bc05340
LumAI <zurichixbt@gmail.com>
2025-06-16 23:07:27 +0200

    Add components/wallet-button.tsx

---
+++ components/wallet-button.tsx
@@ -0,0 +1,9 @@
+'use client';
+import { useWallet } from '@solana/wallet-adapter-react';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+
+export function WalletButton() {
+  return (
+    <WalletMultiButton className="!bg-gradient-to-r !from-purple-600 !to-blue-600 hover:!from-purple-700 hover:!to-blue-700 !rounded-lg !font-semibold !transition-all !duration-200" />
+  );
+}
